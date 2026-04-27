5ea8a83fb3863670acaa4a4fcdf59cb331596c9c
amblighty <david.vielmeyer@outlook.de>
2026-04-27 22:32:58 +0200

    changed comma

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
 return(
     <main>
         <Breadcrumbs breadcrumbs={[
-            { label: 'Invoices', href: '/dashboard/invoices' }
+            { label: 'Invoices', href: '/dashboard/invoices' },
             { 
             label: 'Create Invoice',
             href: '/dashboard/invoices/create',
